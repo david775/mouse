890fdf59ce80cd5536aa3e04bd98a84f75d9b7ae
Hector David S¿egura <hsdsegura6@gmail.com>
2019-05-03 21:30:57 -0500

    acercándose a la meta

--- click.js
+++ click.js
@@ -1,20 +1,51 @@
-var mou=document.addEventListener("mouseover", escuchar);
-var se=document.addEventListener("mousedown", oir);
+var se=document.addEventListener("mousedown", presiona);
+var mou=document.addEventListener("mousmove",mueve);
+var se=document.addEventListener("mouseup", suelta);
 var cuadrito=document.getElementById("click");
 var papel=cuadrito.getContext("2d");
-function mouse(evt){
- return{
-     var x= evt.clientX
-     var y= evt.clientY
- }   
+var x;
+var y;
+var colorcito="#AFA";
+var color = "clack"
+dibujarLinea("color", 0, 0, 300, 0, papel);
+dibujarLinea("color", 300, 0, 300, 300, papel);
+dibujarLinea("color", 300, 300, 0, 300, papel);
+dibujarLinea("color", 0, 300, 0, 0, papel);
+switch (MouseEvent) {
+    case a:
+        
+        break;
+
+    default:
+        break;
 }
 
-function oir (a){
-    console.log(a)
-}
-function escuchar(d) {
-    console.log (d)
-}
+    function presiona(a){
+        console.log("x", a.clientX)
+        console.log("y", a.clientY)    
+        var clx=a.clientX
+        var cly=a.clientY
+     
+        dibujarLinea(colorcito, clx, cly, 0, 0, papel)
+         
+    }
+    function mueve(d) {
+        console.log ("y", d.clientY)
+        console.log ("x", d.clientX)        
+        var clx=d.clientX
+        var cly=d.clientY
+        var colorcito="#AFA";
+        dibujarLinea(colorcito, clx, cly, 0, 0, papel)
+    }
+    function suelta(d) {
+        console.log ("y", d.clientY)
+        console.log ("x", d.clientX)        
+        var clx=d.clientX
+        var cly=d.clientY
+        var colorcito="#AFA";
+        dibujarLinea(colorcito, clx, cly, 0, 0, papel)
+    }
+
 
 function dibujarLinea(color, x_ini, y_ini, x_final, y_final, lienzo)
 { 
@@ -26,8 +57,9 @@ function dibujarLinea(color, x_ini, y_ini, x_final, y_final, lienzo)
     lienzo.stroke();
     lienzo.closePath();   
 }
-function dibijarmouse(evento) {
+function dibijarmouse() {
     var colorcito="#AFA";
-    dibujarLinea(colorcito, X, Y, X, Y, papel);
+    dibujarLinea(colorcito, clx, cly, 0, 0, papel);
     
 }
+
